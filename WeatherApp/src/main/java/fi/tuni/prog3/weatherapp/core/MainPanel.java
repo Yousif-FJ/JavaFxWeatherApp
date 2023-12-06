deef1b26f13f8e93b4c568b08f0be5df5c9f1fd4
David Lakos <lakosd@lakosd.net>
2023-12-06 14:03:25 +0200

    Add weather icon layout

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/core/MainPanel.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/core/MainPanel.java
@@ -2,6 +2,8 @@ package fi.tuni.prog3.weatherapp.core;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -14,12 +16,17 @@ public class MainPanel {
         rightHBox.setPrefWidth(700);
         rightHBox.setStyle("-fx-background-color: #b1c2d4;");
 
-        var currentTemperature = new Label("0");
+        var currentTemperature = new Label("0°C");
         currentTemperature.setFont(new Font(24));
+
+        String iconPath = "/weathericons/01d@2x.png";
+        Image iconImage = new Image(getClass().getResource(iconPath).toExternalForm());
+        ImageView iconImageView = new ImageView(iconImage);
+
         var currentWeatherBox = new HBox();
         currentWeatherBox.setPadding(new Insets(10));
         currentWeatherBox.setPrefHeight(194);
-        currentWeatherBox.getChildren().addAll(currentTemperature);
+        currentWeatherBox.getChildren().addAll(currentTemperature, iconImageView);
 
         var forecastBox = new HBox();
         currentWeatherBox.setPrefHeight(194);
