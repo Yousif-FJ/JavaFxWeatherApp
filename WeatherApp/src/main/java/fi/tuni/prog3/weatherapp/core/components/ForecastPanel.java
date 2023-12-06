3543fb4114dc845537b7aa8f4d63cb57729532cf
Yousef-FJ <47510107+Yousif-FJ@users.noreply.github.com>
2023-12-06 19:29:45 +0200

    Add basic layout for forecast panel

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/core/components/ForecastPanel.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/core/components/ForecastPanel.java
@@ -1,6 +1,15 @@
 package fi.tuni.prog3.weatherapp.core.components;
 
+import java.time.LocalDate;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class ForecastPanel {
 
@@ -8,6 +17,35 @@ public class ForecastPanel {
         var forecastBox = new HBox();
         forecastBox.setStyle("-fx-background-color: #7AEAF1;");
         forecastBox.setPrefHeight(168);
+
+        LocalDate date = LocalDate.now(); 
+        for (int i = 0; i < 5; i++) {
+            var weekdayShorthand =  date.getDayOfWeek().toString().substring(0, 3);
+            // System.out.print(weekdayShorthand + "\n");
+            date = date.plusDays(1);
+
+            forecastBox.getChildren().add(createForecastElement());
+        }
+
         return forecastBox;
+    }
+
+    private VBox createForecastElement(){
+        var mainVBox = new VBox();
+        mainVBox.setAlignment(Pos.CENTER);
+        mainVBox.setPadding(new Insets(0, 20, 0, 20));
+
+        var dayLabel = new Label("Today");
+        
+
+        String iconPath = "/weathericons/01d@2x.png";
+        var iconImage = new Image(getClass().getResource(iconPath).toExternalForm());
+        var iconImageView = new ImageView(iconImage);
+
+        var maxMinTempLabel = new Label("↑-2  ↓-3");
+        maxMinTempLabel.setFont(new Font(24));
+
+        mainVBox.getChildren().addAll(dayLabel, iconImageView, maxMinTempLabel);
+        return mainVBox;
     }
 }
